a844b7ec3314bbd2b388ddec1071ccb8e4233c25
Zi2le <onaopemipoojedokun@gmail.com>
2023-08-21 07:49:45 +0100

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      screens: {
-        ms: '370px',
-        sm: '640px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
-      },
+      // screens: {
+      //   ms: '370px',
+      //   sm: '640px',
+      //   md: '768px',
+      //   lg: '976px',
+      //   xl: '1440px',
+      // },
     },
   },
   plugins: [],
